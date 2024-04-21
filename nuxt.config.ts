a4fd3410924664a1bb05ade36ea3f63521676a95
FroseWong <tibafrose@gmail.com>
2024-04-21 18:17:14 +0800

    update vueuse swiper tailwindcss, npm run start

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
+// import { defineNuxtModule } from 'nuxt' // swiper example
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@pinia/nuxt"],
+  modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss", "@vueuse/nuxt", "nuxt-swiper"],
   alias: {
     pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs",
   },
