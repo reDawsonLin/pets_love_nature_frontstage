bc36d768e4eab589c9655e803dbd386a688d7c3e
Dawson <redawson357@gmail.com>
2024-04-29 07:57:22 +0800

    temporary: navbar 暫存，unocss studing

--- components/layout/footer.vue
+++ components/layout/footer.vue
@@ -1,9 +1,7 @@
 <script setup></script>
 
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <div><h1 class="font-semibold text-4xl">Footer</h1></div>
 </template>
 
 <style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <script setup></script>
 
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <div class="border border-violet-700 h-dvh">
+  123123
+  </div>
 </template>
+
 
 <style lang="scss" scoped></style>
