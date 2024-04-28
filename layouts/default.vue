45754888a935c3983fe7b340bebb36686d996708
Dawson <redawson357@gmail.com>
2024-04-29 07:57:22 +0800

    temporary: navbar 暫存，unocss studing

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <div class="flex flex-col h-dvh border border-red-500">
+    <LayoutNavbar />
+
+    
+    <slot></slot>
+
+    <LayoutFooter />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
