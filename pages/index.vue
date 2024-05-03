0753152925c9d2b4e7b0adf98b917c48f8002e4d
reDawson <redawson357@gmail.com>
2024-05-03 16:41:17 +0800

    ﻿feat: - google font setting - css import

--- pages/index.vue
+++ pages/index.vue
@@ -21,26 +21,61 @@ const dummy_banner = [
     title: "毛孩天然美食 享受鮮食饗宴",
     subtitle: "鮮食無負擔，原型肉品無添加，寵愛無疆界",
   },
+  {
+    id: 4,
+    imgUrl: "/img/banner/1.webp",
+    routeLink: "",
+    title: "原型鮮食 頂級饗宴",
+    subtitle: "全人食等級，寵物鮮食新選擇",
+  },
+  {
+    id: 5,
+    imgUrl: "/img/banner/2.webp",
+    routeLink: "",
+    title: "全人食等級 寵愛每一口",
+    subtitle: "原塊肉品，天然凍乾，保留原味原香",
+  },
+  {
+    id: 6,
+    imgUrl: "/img/banner/3.webp",
+    routeLink: "",
+    title: "毛孩天然美食 享受鮮食饗宴",
+    subtitle: "鮮食無負擔，原型肉品無添加，寵愛無疆界",
+  },
 ];
-
-
 </script>
 
 <template>
-  <div p="x-0.75rem t-1.25rem" class="flex-grow-1 border">
+  <div p="x-0.75rem t-1.25rem" class="flex-grow-1 border bg-second-200">
+    <!-- <img
+      src="/img/banner/1.webp"
+      alt="banner"
+      class="aspect-350/512"
+      rounded="t-80px b-20px"
+      object="cover center"
+    /> -->
     <Swiper
+      space-between="12"
       :modules="[SwiperPagination]"
       :pagination="{ dynamicBullets: true }"
-      class="aspect-350/512"
     >
-      <SwiperSlide v-for="banner in dummy_banner" :key="banner.id">
+      <SwiperSlide
+        v-for="banner in dummy_banner"
+        :key="banner.id"
+        class="relative"
+      >
         <img
           :src="banner.imgUrl"
           alt="banner"
-          class="h-100% w-100%"
-          object="cover center"
+          class="aspect-350/512"
           rounded="t-80px b-20px"
-        >
+          object="cover center"
+        />
+
+        <div p-1rem round="t-1rem" border border-red-7>
+          <h3 class="text-2rem">{{ banner.title }}</h3>
+          <p>{{ banner.subtitle }}</p>
+        </div>
       </SwiperSlide>
     </Swiper>
   </div>
--- pages/product/[id]/index.vue
+++ pages/product/[id]/index.vue
@@ -1,3 +1,7 @@
+<script setup></script>
+
 <template>
-    <div></div>
+  <div></div>
 </template>
+
+<style lang="scss" scoped></style>
