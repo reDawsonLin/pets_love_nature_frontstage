1b84f65d7862df68d316b4a07ef3d5a3ee9792d4
reDawson <redawson357@gmail.com>
2024-05-06 17:51:45 +0800

    temporary: product category hover effect gradient

--- pages/index.vue
+++ pages/index.vue
@@ -43,10 +43,63 @@ const dummy_banner = [
     subtitle: "鮮食無負擔，原型肉品無添加，寵愛無疆界",
   },
 ];
+
+const dummy_feature = [
+  {
+    id: 1,
+    iconName: "advance_nature",
+    content1: "100%",
+    content2: "純天然原料",
+  },
+  {
+    id: 2,
+    iconName: "advance_good",
+    content1: "無防腐劑",
+    content2: "無誘食劑",
+  },
+  {
+    id: 3,
+    iconName: "advance_noGluten",
+    content1: "無麩質",
+    content2: "無添加",
+  },
+  {
+    id: 4,
+    iconName: "advance_noAdd",
+    content1: "人食等級",
+    content2: "安心食用",
+  },
+];
+
+const dummy_category = [
+  {
+    id: 1,
+    title: "原塊凍乾",
+    subtitle: "Freeze Dried Chunk",
+    description:
+      "台灣自有工廠生產，不加任何調味料防腐劑、堅持保有最原始的營養。低脂低熱量，結構完整鬆軟，輕鬆撥小塊、壓粉，不須加水還原就可以輕鬆食用！",
+    remind: "",
+    bgUrl: "bg-[url(/img/home-2-1.webp)]",
+    routeUrl: "",
+  },
+  {
+    id: 2,
+    title: "客製鮮食",
+    subtitle: "Customized Fresh Food",
+    description:
+      "100% 純手工製作，根據犬貓不同階段需求，虚弱、挑食、疾病調養等，客製調配最適合的鮮食。",
+    remind: "*鮮食為食品，醫療仍須遵照醫囑，並請依照醫師指示食用。",
+    bgUrl: "bg-[url(/img/home-2-2.webp)]",
+    routeUrl: "",
+  },
+];
 </script>
 
 <template>
-  <div p="x-0.75rem t-1.25rem" class="flex-grow-1 border bg-second-200">
+  <section
+    class="banner_list flex-grow-1 bg-second-200"
+    p="x-0.75rem t-1.25rem"
+  >
     <Swiper
       space-between="12"
       :modules="[SwiperPagination]"
@@ -66,14 +119,71 @@ const dummy_banner = [
         />
 
         <div
-          class="absolute bottom-40px left-50% mx-auto w-[calc(100%-2rem)] flex flex-col translate-x--50% items-center gap-0.5rem rounded-t-1rem bg-second-200 p-1rem text-center text-neutral-600"
+          class="absolute bottom-50px left-50% mx-auto w-[calc(100%-2rem)] flex flex-col translate-x--50% items-center gap-0.5rem rounded-t-1rem bg-second-200 p-1rem text-center text-neutral-600"
         >
           <h3 class="text-2rem">{{ banner.title }}</h3>
           <p class="text-balance">{{ banner.subtitle }}</p>
         </div>
       </SwiperSlide>
     </Swiper>
-  </div>
+  </section>
+
+  <section class="brand_info" p="x-0.75rem y-3rem">
+    <header class="flex flex-col items-center mb-1rem">
+      <SvgIcon name="logo_pink" class="w-5rem" />
+      <h3>Brand 特色</h3>
+    </header>
+
+    <ul class="box_feature grid grid-cols-2">
+      <template v-for="item in dummy_feature" :key="item.id">
+        <li class="feature px-0.75rem py0.5rem text-neutral-800 text-center">
+          <div
+            class="wrapper_icon flex justify-center items-center mb-0.25rem aspect-1/1 rounded-1.5rem bg-second-400 p-1.75rem shadow-[-0.25rem_0.25rem_0.5rem_rgba(210, 148, 107,0.3)]"
+          >
+            <SvgIcon :name="item.iconName" class="w-5rem" />
+          </div>
+          <p text-1.25rem>{{ item.content1 }}</p>
+          <p text-1.25rem>{{ item.content2 }}</p>
+        </li>
+      </template>
+    </ul>
+  </section>
+
+  <section class="product_category">
+    <ul class="flex flex-col gap-1.5rem py-2rem px-0.75rem">
+      <li v-for="item in dummy_category" :key="item.id" class="flex gap-1.5rem">
+        <div
+          class="box_title write-vertical-left flex gap-1rem justify-center items-center"
+        >
+          <p class="text-2.5rem tracking-1rem">{{ item.title }}</p>
+          <p class="text-0.75rem">{{ item.subtitle }}</p>
+        </div>
+
+        <div
+          class="relative rounded-1.5rem aspect-257/358 flex-grow-1"
+          :class="`${item.bgUrl}`"
+          bg="center [length:auto_100%] no-repeat"
+        >
+          <div
+            class="absolute inset-0 px-1rem py-0.75rem flex flex-col justify-end bg-gradient-to-t from-[#edeef098]"
+          >
+        <!-- from-[rgb(237_ 238_240/.98)] -->
+          <!-- bg-gradient-to-t from-[#edeef0] from-98% -->
+            <!-- background-image: linear-gradient(to bottom, rgba(255, 255, 255,
+            0.8), rgba(255, 255, 255, 0)); -->
+            <!-- linear-gradient(0deg, rgba(237,238,240,0.861782212885154) 0%, rgba(237,238,240,1) 98%); -->
+
+            <p class="text-1rem">{{ item.description }}</p>
+            <p v-show="item.remind" class="text-0.75rem">{{ item.remind }}</p>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </section>
+
+  <section class="product_suggest"></section>
+
+  <section class="comment_list"></section>
 </template>
 
 <style scoped>
