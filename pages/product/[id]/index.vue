ca72bca5d0b00fa0aa56a8bddf3a7db4155449ba
snail524 <immigrant524@gmail.com>
2024-06-02 22:52:55 +0800

    feat: 商品詳細頁PC第一版完成，還缺RWD跟串接API，細部切版也要修

--- pages/product/[id]/index.vue
+++ pages/product/[id]/index.vue
@@ -9,7 +9,7 @@ const productData = ref([
     inStock: 50,
     productId: {
       prductNumber: "A001",
-      title: "鮮嫩雞胸肉鮮食罐頭",
+      title: "鮮嫩雞胸肉鮮食罐頭1",
       imageGallery: [
         {
           imgUrl: "https://images.unsplash.com/photo-1597843786411-a7fa8ad44a95?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -26,7 +26,7 @@ const productData = ref([
     inStock: 50,
     productId: {
       prductNumber: "A001",
-      title: "鮮嫩雞胸肉鮮食罐頭",
+      title: "鮮嫩雞胸肉鮮食罐頭2",
       imageGallery: [
         {
           imgUrl: "https://images.unsplash.com/photo-1597843786411-a7fa8ad44a95?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -64,16 +64,16 @@ const addToCart = () => {
 <template>
   <div class="bg-white">
     <div class="mx-auto max-w-4xl px-4 py-16 lg:max-w-7xl lg:px-8 sm:px-6 sm:py-24">
-      <div class="header h-auto md:h-[700px]">
+      <div class="header h-auto md:h-[773px]">
         <div class="product-imgs grid grid-cols-1 md:grid-cols-2">
-          <div class="imgs w-[100%]">
-            <div>
+          <div class="imgs w-[90%]">
+            <div class="mb-[16px] h-[637px]">
               <img
-class="ma"
+                class="ma h-[100%] object-contain"
                 src="https://thumbnail7.coupangcdn.com/thumbnails/remote/492x492ex/image/retail/images/12726835984140-c4068191-7291-456e-b6b4-792140c83051.png"
                 alt="">
             </div>
-            <div class="h-[120px] w-[100%] flex justify-between">
+            <div class="h-[120px] w-[90%] flex justify-between">
               <div class="w-[24px] flex items-center bg-[#E5E5E5]">
 
                 <img src="/assets/img/icon/icon-chevron_left.svg" alt="">
@@ -99,8 +99,8 @@ class="ma"
           <div class="product-imgs grid grid-cols-6 flex flex-col">
             <h2 class="mb-[16px]">鮮嫩雞胸肉凍乾</h2>
             <p class="mb-[16px] text-[#525252] font-light">新鮮雞胸肉，符合人食等級，富含高品質蛋白質，提供毛孩維持健康體愛所需的重要營養素。</p>
-            <p class="mb-[32px]">NT$ <span class="font-size-[48px] font-300">300 </span></p>
-            <div class="weight mb-[16px] flex">
+            <p class="mb-[64px]">NT$ <span class="font-size-[48px] font-300">300 </span></p>
+            <div class="weight mb-[24px] flex">
               <div
                 class="mr-4 h-[45px] w-[100px] flex items-center justify-center b-rd-8px bg-[#F43F5E] pb-[8px] pt-[8px] text-center font-size-[24px] text-white font-200">
                 100g</div>
@@ -111,14 +111,14 @@ class="ma"
             <div>
               <div class="mb-[16px] w-[140px] flex justify-between b b-[#E5E5E5] b-rd-8px b-solid p-[8px]">
                 <img class="" src="/assets/img/icon/icon-remove.svg" alt="" style="fill: red;">
-                <div class="font-size-[20px]">2</div>
+                <div class="font-size-[20px]">1</div>
                 <img class="" src="/assets/img/icon/icon-add.svg" alt="" style="fill: red;">
 
               </div>
 
             </div>
 
-            <div class="md:flew-row mb-[48px] flex flex-col md:flex-row">
+            <div class="md:flew-row mb-[64px] flex flex-col md:flex-row">
               <div
                 class="mx-auto mb-[12px] h-[60px] w-[100%] flex cursor-pointer items-center justify-center rounded bg-[#E5E5E5] text-black lg:mx-0 lg:mr-4 md:mb-[0] lg:h-[60px] lg:w-64 md:w-[258px]">
                 <img class="mr-3" src="/assets/img/icon/icon-cart-bgE5.svg" alt="" style="fill: red;">
@@ -165,8 +165,9 @@ class="ma"
 
       </div>
 
-      <div class="content">
+      <div class="content mb-[120px]">
         <img
+          class="ma"
           src="https://thumbnail7.coupangcdn.com/thumbnails/remote/492x492ex/image/retail/images/12726835984140-c4068191-7291-456e-b6b4-792140c83051.png"
           alt="" >
 
@@ -218,7 +219,7 @@ class="ma"
           </div>
         </div>
         <div>
-          <div class="h-[50px] bg-[#E5E5E5] px-[16px] py-[8px] font-size-[24px] text-[#525252] font-200">
+          <div class="mb-[48px] h-[50px] bg-[#E5E5E5] px-[16px] py-[8px] font-size-[24px] text-[#525252] font-200">
             相關商品
           </div>
           <!-- test -->
@@ -226,13 +227,13 @@ class="ma"
           <div class="grid grid-cols-3 mt-6 gap-x-6 gap-y-10">
 
             <div
-v-for="(product, index) in productData" :key="product._id"
+              v-for="(product) in productData" :key="product._id"
               class="group product relative border b-rd-2xl pb-4 pl-2 pr-2">
               <div
-v-if="product.productId.imageGallery.length > 0"
+                v-if="product.productId.imageGallery.length > 0"
                 class="aspect-h-1 aspect-w-1 lg:aspect-none relative w-full overflow-hidden rounded-md bg-gray-200 lg:h-80 group-hover:opacity-75">
                 <img
-:src="product.productId.imageGallery[0].imgUrl.trim()"
+                  :src="product.productId.imageGallery[0].imgUrl.trim()"
                   :alt="product.productId.imageGallery[0].altText"
                   class="h-full w-full object-cover object-center lg:h-full lg:w-full">
                 <div class="absolute right-2 top-2">
@@ -246,12 +247,8 @@ v-if="product.productId.imageGallery.length > 0"
                 <div>
                   <h3 class="text-sm text-gray-700">
                     <a href="">
-                      <!-- <span aria-hidden="true" class="absolute inset-0">
                         {{ product.productId.title }}
-                      </span> -->
-
                     </a>
-                    <!-- {{ product.productId.imageGallery[0].imgUrl}} -->
 
                   </h3>
                   <p class="mt-1 text-sm text-gray-500">NT$ {{ product.price }}</p>
