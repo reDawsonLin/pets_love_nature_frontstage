94fc4d0889d3ef98d02f62d4cbf16cb528850281
reDawson <redawson357@gmail.com>
2024-06-05 11:46:41 +0800

    feat: step 1 切版完成

--- pages/checkout/step1.vue
+++ pages/checkout/step1.vue
@@ -3,18 +3,6 @@ const show_cart = () => {
   console.log("show cart");
 };
 
-// 選擇滑窗
-const show_modal = ref(false);
-const openModal = () => {
-  document.querySelector("body").classList.add("stopScroll");
-  show_modal.value = true;
-};
-
-const closeModal = () => {
-  document.querySelector("body").classList.remove("stopScroll");
-  show_modal.value = false;
-};
-
 const dummy_cart = ref([
   {
     id: 1,
@@ -67,115 +55,139 @@ const param_post = ref({
   receipt: "",
   payment_method: "",
 });
+
+// 選擇滑窗
+const show_modal = ref(false);
+const openModal = () => {
+  console.log("in open Modal");
+  document.querySelector("body").classList.add("stopScroll");
+  show_modal.value = true;
+};
+
+const closeModal = () => {
+  console.log("in close Modal");
+  document.querySelector("body").classList.remove("stopScroll");
+  show_modal.value = false;
+};
+
+const { width: window_width } = useWindowSize();
 </script>
 
 <template>
-  <div class="bg_blur bg-neutral-500/80 fixed inset-0 z-10 backdrop-blur-5px" />
-
-  <div
-    class="modal_cart fixed top-50% left-50% z-11 translate--50% max-h-1087px h-80% w-[calc(100%-2rem)] max-w-1064px"
-  >
-    <SvgIcon
-      name="close"
-      class="absolute right--4px top--2.5rem z-11 text-neutral-50 w-2rem h-2rem text-neutral-50"
-    />
-
-    <section
-      class="bg-neutral-50 overflow-y-auto h-100% py-1.5rem px-1rem rounded-0.5rem flex flex-col"
+  <Teleport to="body">
+    <div
+      class="bg_blur bg-neutral-500/80 fixed inset-0 z-10 backdrop-blur-5px opacity-0 pointer-events-none"
+      :class="show_modal ? 'opacity-100 pointer-events-unset' : ''"
+      @click.self="closeModal()"
     >
-      <p
-        class="hidden bg-neutral-200 text-neutral-600 text-1.25rem p-0.5rem rounded-0.5rem"
+      <div
+        class="modal_cart fixed top-50% left-50% z-11 translate--50% max-h-80% h-fit w-[calc(100%-2rem)] max-w-1064px lg:(max-h-1087px)"
       >
-        商品
-      </p>
+        <SvgIcon
+          name="close"
+          class="absolute right--4px top--2.5rem z-11 text-neutral-50 w-2rem h-2rem text-neutral-50 cursor-pointer"
+          @click="closeModal()"
+        />
 
-      <table>
-        <thead>
-          <tr class="thead_tr">
-            <th
-              class="rounded-0.25rem text-1.25rem lg:(text-1rem rounded-l-0.25rem w-35%)"
+        <section
+          class="bg-neutral-50 overflow-y-auto h-100% py-1.5rem px-1rem rounded-0.5rem flex flex-col lg:(py-2.25rem px-2rem)"
+        >
+          <table>
+            <thead>
+              <tr
+                class="thead_tr text-neutral-600 bg-neutral-200 lg:(bg-second-400)"
+              >
+                <th
+                  class="rounded-0.25rem text-1.25rem lg:(text-1rem rounded-l-0.25rem w-37%)"
+                  :colspan="window_width < 1024 ? 1 : 2"
+                >
+                  商品
+                </th>
+                <th class="hidden lg:(table-cell)">單價</th>
+                <th class="hidden lg:(table-cell)">數量</th>
+                <th class="hidden lg:(table-cell) lg:(rounded-r-0.25rem)">
+                  總計
+                </th>
+              </tr>
+            </thead>
+
+            <tbody class="">
+              <tr
+                v-for="item in dummy_cart"
+                :key="item.id"
+                class="tbody_tr mb-1.5rem"
+              >
+                <td class="td_img mr-1rem lg:(min-w-76px)">
+                  <!-- class="w-3.75rem h-3.75rem object-cover object-center" -->
+                  <img
+                    class="h-100% object-cover object-center lg:(w-3.75rem h-3.75rem)"
+                    src="@/assets/img/product-1.png"
+                    alt="product image"
+                  />
+                </td>
+                <td class="td_content">
+                  <p class="line-clamp-2">
+                    {{ item.productName }}
+                  </p>
+                </td>
+
+                <td class="td_price lg:(text-center)">
+                  <p class="">
+                    NT$
+                    <span class="ml-0.25rem text-1.5rem line-height-120%">{{
+                      addThousandPoint(item.price)
+                    }}</span>
+                  </p>
+                </td>
+
+                <td class="td_amount">
+                  <p
+                    class="flex justify-end relative top-2px before:(content-['x']) lg:(text-1.5rem before:(content-empty) justify-center)"
+                  >
+                    {{ item.amount }}
+                  </p>
+                </td>
+
+                <td class="td_total mt-1.5rem sm:(mt-0.5rem) lg:(mt-0)">
+                  <p
+                    class="text-rose-500 flex justify-end items-end lg:(justify-center)"
+                  >
+                    NT$
+                    <span class="ml-0.25rem text-1.5rem line-height-120%">{{
+                      addThousandPoint(item.price * item.amount)
+                    }}</span>
+                  </p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+
+          <div
+            class="lg:(flex justify-end rounded-0.5rem bg-neutral-200 p-1rem)"
+          >
+            <div
+              class="flex items-end justify-center gap-1rem rounded-0.5rem p-1.5rem border border-neutral-200 lg:(px-1.5rem py-1rem bg-neutral-50 border-none)"
             >
-              商品
-            </th>
-            <th class="hidden">單價</th>
-            <th class="hidden">數量</th>
-            <th class="hidden lg:(rounded-r-0.25rem)">總計</th>
-          </tr>
-        </thead>
-
-        <tbody class="">
-          <tr v-for="item in dummy_cart" :key="item.id" class="tbody_tr">
-            <td class="px-1rem py-4.5rem">
-              <div class="box_td flex items-center gap-1rem">
-                <img
-                  class="w-3.75rem h-3.75rem object-cover object-center"
-                  src="@/assets/img/product-1.png"
-                  alt="product image"
-                />
-                <p class="line-clamp-2">
-                  {{ item.productName }}
-                </p>
-              </div>
-            </td>
-            <td class="text-center">
-              <p class="">
-                NT$
-                <span class="ml-0.25rem text-1.5rem line-height-120%">{{
-                  addThousandPoint(item.price)
-                }}</span>
-              </p>
-            </td>
-            <td class="text-1.5rem text-center">{{ item.amount }}</td>
-            <td class="text-center">
+              <p class="text-1.25rem text-neutral-600">總金額</p>
               <p class="text-rose-500">
                 NT$
                 <span class="ml-0.25rem text-1.5rem line-height-120%">{{
-                  addThousandPoint(item.price * item.amount)
+                  addThousandPoint(totalPrice(dummy_cart))
                 }}</span>
               </p>
-            </td>
-          </tr>
-        </tbody>
-
-        <tfoot></tfoot>
-      </table>
-
-      <!-- <ul class="mb-1.5rem">
-      <li class="flex flex-col gap-1.5rem py-1rem px-0.5rem">
-        <div class="flex"></div>
-  
-        <div class="flex items-end justify-end gap-0.25rem pr-0.5rem">
-          <p class="text-neutral-600">總計</p>
-          <p class="text-rose-500">
-            NT$
-            <span class="ml-0.25rem text-1.5rem line-height-120%">{{
-              addThousandPoint(450)
-            }}</span>
-          </p>
-        </div>
-      </li>
-    </ul> -->
-
-      <div
-        class="flex items-end justify-center gap-1rem rounded-0.5rem p-1.5rem border border-neutral-200"
-      >
-        <p class="text-1.25rem text-neutral-600">總金額</p>
-        <p class="text-rose-500">
-          NT$
-          <span class="ml-0.25rem text-1.5rem line-height-120%">{{
-            addThousandPoint(totalPrice(dummy_cart))
-          }}</span>
-        </p>
+            </div>
+          </div>
+        </section>
       </div>
-    </section>
-  </div>
+    </div>
+  </Teleport>
 
   <div
     class="wrapper_step1 mx-auto max-w-1076px flex flex-col gap-2rem px-0.75rem py-3rem"
   >
     <section
       class="group info_cart transition-background flex flex-col cursor-pointer gap-1.5rem rounded-1rem bg-second-400 px-1rem py-1.5rem text-neutral-600 transition-colors hover:(bg-neutral-600 text-neutral-50) lg:(px-1.75rem py-2.5rem)"
-      @click="show_cart()"
+      @click="openModal()"
     >
       <SvgIcon
         name="cart"
@@ -395,21 +407,11 @@ const param_post = ref({
   }
 }
 
+/* // modal part -------
+.bg_blur {
+} */
+
 .thead_tr {
-  @apply text-neutral-600 bg-neutral-200;
-
-  @media screen and (min-width: 1024px) {
-    @apply bg-second-400;
-
-    /* display: grid;
-    grid-template-areas:
-      "address cart"
-      "address  payment"
-      "button   button";
-    grid-template-columns: repeat(1fr, 0.8fr);
-    gap: 1.5rem; */
-  }
-
   > th {
     @apply font-400 pt-1rem pb-0.625rem;
   }
@@ -417,5 +419,47 @@ const param_post = ref({
 
 .tbody_tr {
   display: grid;
+  grid-template-areas:
+    "img content content"
+    "img price amount"
+    "total total total";
+
+  grid-template-columns: 1fr 1fr 1fr;
+  padding: 1rem 0.5rem;
+
+  @media screen and (min-width: 640px) {
+    grid-template-areas:
+      "img content content"
+      "img price amount"
+      "img total total";
+  }
+
+  @media screen and (min-width: 1024px) {
+    display: table-row;
+  }
+
+  > td {
+    @apply lg:([&:nth-child(1)]-pr-0 px-1rem py-4.5rem);
+  }
+}
+
+.td_img {
+  grid-area: img;
+}
+
+.td_content {
+  grid-area: content;
+}
+
+.td_price {
+  grid-area: price;
+}
+
+.td_amount {
+  grid-area: amount;
+}
+
+.td_total {
+  grid-area: total;
 }
 </style>
