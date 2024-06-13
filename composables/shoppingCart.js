c090f630bb60e014b19f518da16905b2111d3708
FroseWong <tibafrose@gmail.com>
2024-06-13 23:05:32 +0800

    fix 把給Dawson看的問題先刪除

--- composables/shoppingCart.js
+++ composables/shoppingCart.js
@@ -1,8 +1,6 @@
 import { storeToRefs } from "pinia";
 import { useStoreLogin } from "~/stores/storeLogin";
 import Swal from 'sweetalert2'
-import { useStoreCart } from "~/stores/storeCart";
-const storeCart = useStoreCart();
 
 const noLoginTestObj = {
   "shoppingCart": [
@@ -104,10 +102,6 @@ export const useShoppingCart = async () => {
     })
 
     const filteredArr = transformArray.filter(eachData => eachData._id)
-    console.log('filteredArr', filteredArr);
-    console.log('storeCart.cartArr.value', storeCart.cartArr.value);
-    storeCart.cartArr.value = filteredArr;
-    console.log('storeCart.cartArr.value', storeCart.cartArr.value);
     return filteredArr;
   };
 
