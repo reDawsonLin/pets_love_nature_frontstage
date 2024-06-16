64684570c0a276f302d502e382481eef0e74ce9e
snail524 <immigrant524@gmail.com>
2024-06-16 11:17:02 +0800

    feature: sidebar元件化完成

---
+++ components/sidebar/order.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+<template>
+    <!--  外層 div RWD 參考 -->
+    <!--  <div class="w-[100%] flex flex-col grid-justify-start grid-items-center md:mt-[100px] md-w-[30%]">  -->
+    <ul class="w-[100%] flex flex-row justify-center text-center md-w-[80%] md:flex-col">
+        <li class="m-2">
+        <a href="#" class="block p-2 hover:bg-[#fdd8bf]">個人資訊</a>
+        </li>
+        <li class="m-2">
+        <a href="#" class="block p-2 hover:bg-[#fdd8bf]">訂單資訊</a>
+        </li>
+        <li class="m-2">
+        <a href="#" class="block p-2 hover:bg-[#fdd8bf]">收藏商品</a>
+        </li>
+        <li class="m-2">
+        <a href="#" class="block p-2 hover:bg-[#fdd8bf]">聊聊紀錄</a>
+        </li>
+    </ul>
+</template>
--- pages/member/index.vue
+++ pages/member/index.vue
@@ -95,7 +95,7 @@ onMounted(() => {
 <template>
   <div class="member mx-auto container md-flex">
     <div class="sidebar w-[100%] flex flex-col grid-justify-start grid-items-center md:mt-[100px] md-w-[30%]">
-      <ul class="w-[100%] flex flex-row justify-center text-center md-w-[80%] md:flex-col">
+      <!-- <ul class="w-[100%] flex flex-row justify-center text-center md-w-[80%] md:flex-col">
         <li class="m-2">
           <a href="#" class="block p-2 hover:bg-[#fdd8bf]">個人資訊</a>
         </li>
@@ -108,7 +108,8 @@ onMounted(() => {
         <li class="m-2">
           <a href="#" class="block p-2 hover:bg-[#fdd8bf]">聊聊紀錄</a>
         </li>
-      </ul>
+      </ul> -->
+      <SidebarOrder />
     </div>
 
     <div class="w-[100%] md-w-[70%]">
